e359466f75494517292554092b5d1d1f22aff0c4
duducesconetto <cesconettoedu@gmail.com>
2022-08-05 04:04:13 +0000

    connect postgresql database from nodes aplication

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -10,7 +10,6 @@ const pool = new Pool({
 });
 
 pool.query(`
-
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
@@ -18,7 +17,6 @@ JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
 ORDER BY teacher;
-
 `)
 
 .then(res => {
