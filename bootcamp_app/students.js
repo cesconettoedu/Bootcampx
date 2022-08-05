0253dbb47486c062e1b3ed8011f6fa56370a0876
duducesconetto <cesconettoedu@gmail.com>
2022-08-05 17:51:26 +0000

    parameterized queries students and teachers

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,14 +8,17 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+              // this will put a default if dont have any arguments
+let cohort = typeof process.argv[2] === 'undefined' ? 'FEB12' : process.argv[2]            
+let limit = typeof process.argv[3] === 'undefined' ? 5 : process.argv[3] 
 
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, [cohort, limit])
 
 .then(res => {
   res.rows.forEach(user => {
